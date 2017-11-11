e94586f848af6114564f05300bd9dcf3d0595046
Marcos T Munhoz <marmunhoz414@gmail.com>
2017-11-11 02:36:02 -0200

    Update setup.php

--- php_scripts/setup.php
+++ php_scripts/setup.php
@@ -1,8 +1,8 @@
 <?php
-    define("DB_HOST", "localhost");
-    define("DB_NAME", "banco_corridas");
-    define("DB_LOGIN", "marcos");
-    define("DB_PASSWD", "98564831");
+    define("DB_HOST", "us-cdbr-iron-east-05.cleardb.net");
+    define("DB_NAME", "heroku_de5dfc1975ecdc6");
+    define("DB_LOGIN", "b7c929ef6f1a1f");
+    define("DB_PASSWD", "fe8607a4");
 
     function format_string(string $str, string $mask) {
         $result = "";
