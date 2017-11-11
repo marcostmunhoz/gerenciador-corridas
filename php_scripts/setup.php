1bade713bac535bf37aa02df476dc7d05ad1e80f
Marcos T Munhoz <marmunhoz414@gmail.com>
2017-11-11 02:43:29 -0200

    Update setup.php

--- php_scripts/setup.php
+++ php_scripts/setup.php
@@ -1,4 +1,7 @@
 <?php
+    ini_set("error_reporting", "true");
+    error_reporting(E_ALL|E_STRCT);
+
     define("DB_HOST", "us-cdbr-iron-east-05.cleardb.net");
     define("DB_NAME", "heroku_de5dfc1975ecdc6");
     define("DB_LOGIN", "b7c929ef6f1a1f");
